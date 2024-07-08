98ab3644ee9e6929016ae6b04bcd43fec22a22cd
Cocostrime <120545424+Cocostrime@users.noreply.github.com>
2024-07-08 17:27:34 +0530

    clear and pop .py

--- set/pop .py
+++ set/pop .py
@@ -1,3 +1,5 @@
 months = set(["January","February","March","April","May","June","July","August","September","October","November","December"])
 months.pop();
 print(months)
+months.clear();
+print(months)
